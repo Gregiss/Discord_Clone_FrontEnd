3b82c5bb3cf8e8647acd9501ba3ab4069b82561d
Alexandre Silva <xande1231231@hotmail.com>
2019-07-29 08:06:10 -0300

    Change let to constant

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 //Login html page
-let login = "<form action=''> <h1>Welcome Back</h1> <p>We're so excited to see you again!</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Login</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Need a account?</span><a id='need' href='#register' style='top: 8px; left: 0px;'>Register</a></span> </form>";
+const login = "<form action=''> <h1>Welcome Back</h1> <p>We're so excited to see you again!</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Login</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Need a account?</span><a id='need' href='#register' style='top: 8px; left: 0px;'>Register</a></span> </form>";
 //Register html page
-let register = "<form action=''> <h1 style='font-size: 30px;'>Create an account</h1> <p>Explore the world</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <span>Repeat password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Register</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Have an account?</span><a id='have' href='#have' style='top: 8px; left: 0px;'>Login</a></span> </form>";
+const register = "<form action=''> <h1 style='font-size: 30px;'>Create an account</h1> <p>Explore the world</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <span>Repeat password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Register</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Have an account?</span><a id='have' href='#have' style='top: 8px; left: 0px;'>Login</a></span> </form>";
 //Splash
-let splash = "<div id='splash'></div>";
+const splash = "<div id='splash'></div>";
 
-let frases = [{frase: "Be and honor the person you are. 🍃✨"}, {frase: "Well I want, well I do, I want you, peace I bring you. 🌻✌"}, {frase: "For those who feel little, I’m sorry. 💥"}, {frase: "Learn to flourish in the adversities of life. 🌺🍃"}, {frase : "Make every moment of your life worthwhile. 🍃✨"}]
+const frases = [{frase: "Be and honor the person you are. 🍃✨"}, {frase: "Well I want, well I do, I want you, peace I bring you. 🌻✌"}, {frase: "For those who feel little, I’m sorry. 💥"}, {frase: "Learn to flourish in the adversities of life. 🌺🍃"}, {frase : "Make every moment of your life worthwhile. 🍃✨"}]
 
-var newFrase = getRandomInt(frases.length);
+const newFrase = getRandomInt(frases.length);
 
-let splashFull = "<div class='splashFull'><video src='assets/splash.webm' loop autoplay></video><span id='frase'>"+frases[newFrase].frase+"</span></div>"
+const splashFull = "<div class='splashFull'><video src='assets/splash.webm' loop autoplay></video><span id='frase'>"+frases[newFrase].frase+"</span></div>"
 
 
 $(document).ready(function(){
