bbac44d937f474d1d98a34c9523881effa0de533
Alexandre Silva <xande1231231@hotmail.com>
2019-07-28 21:32:45 -0300

    Fix

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //Login html page
 let login = "<form action=''> <h1>Welcome Back</h1> <p>We're so excited to see you again!</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Login</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Need a account?</span><a id='need' href='#register' style='top: 8px; left: 0px;'>Register</a></span> </form>";
 //Register html page
-let register = "<form action=''> <h1 style='font-size: 30px;'>Create an account</h1> <p>Explore the world</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <span>Repeat password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Login</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Have an account?</span><a id='have' href='#have' style='top: 8px; left: 0px;'>Login</a></span> </form>";
+let register = "<form action=''> <h1 style='font-size: 30px;'>Create an account</h1> <p>Explore the world</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <span>Repeat password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Register</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Have an account?</span><a id='have' href='#have' style='top: 8px; left: 0px;'>Login</a></span> </form>";
 //Splash
 let splash = "<div id='splash'></div>";
 
