f8bffde1a5385373f682fd82d132637c2a400e94
Alexandre Silva <xande1231231@hotmail.com>
2019-07-30 14:09:15 -0300

    Fix any bugs

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 const icon_friend = "<i class='fas fa-user-friends'></i>";
 const line = "<div class='line'></div>";
-const friends_icon = "<div class='icon friends_icon icon_acess'>"+icon_friend+"</div>"+line+"";
+const friends_icon = "<div class='icon friends_icon icon_acess'>"+icon_friend+"</div><div class='serves'>"+line+"<div class='before'></div></div>";
 const app = "<div class='app'> <div class='before'></div>  </div>";
 const left_bar = "<div class='left_bar'><div class='before'></div></div>"
 const friends_html = "<div class='friends'><div class='po'></div></div>";
@@ -13,6 +13,7 @@ function newServer(name, photo){
     servers.push(server);
     var id = servers.length - 1;
     showServer(name, photo, id);
+    acessServer();
 }
 
 
@@ -22,13 +23,15 @@ function startApp(){
     $(".app .before").before(friends_html);
     $(".app .friends .po").before(search_bar);
     $(".app .left_bar .before").before(friends_icon);
+    for(var i = 0; i < 9; i++){
     newServer("Teste", "https://cdn.discordapp.com/icons/601259152353787905/9425b887d117585171cccdce2f6282cd.png?size=128");
     newServer("Teste", "https://cdn.discordapp.com/icons/574382397035118624/c28fafc7d19c4448ec1c9256e102eaa0.png?size=128");
+    }
     acessServer();
 }
 
 function showServer(name, photo, id){
-    $(".app .left_bar .line").after("<div class='icon server' data-id='"+id+"'><div class='photo' id='server"+id+"'><img class='icon_img' src='"+photo+"'></img></div></div>");
+    $(".app .left_bar .serves .before").after("<div class='icon server' data-id='"+id+"'><div class='photo' id='server"+id+"'><img class='icon_img' src='"+photo+"'></img></div></div>");
 }
 
 function acessServer(){
