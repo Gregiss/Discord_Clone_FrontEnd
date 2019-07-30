18c9bbb1aa637dcda0ccc1fe1be9eb4e3865c070
Alexandre Silva <xande1231231@hotmail.com>
2019-07-30 12:04:32 -0300

    Fix

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //Login html page
-const login = "<form action=''> <h1>Welcome Back</h1> <p>We're so excited to see you again!</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Login</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Need a account?</span><a id='need' href='#register' style='top: 8px; left: 0px;'>Register</a></span> </form>";
+const login = "<form action=''> <h1>Welcome Back</h1> <p>We're so excited to see you again!</p> <span>E-mail</span> <input id='email' type='text'> <span>Password</span> <input id='password' type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Login</button> <span id='need'><span style='font-size: 14px;line-height: 16px; top: 20px;'>Need a account?</span><a id='need' href='#register' style='top: 8px; left: 0px;'>Register</a></span> </form>";
 //Register html page
-const register = "<form action=''> <h1 style='font-size: 30px;'>Create an account</h1> <p>Explore the world</p> <span>E-mail</span> <input type='text'> <span>Password</span> <input type='password'> <span>Repeat password</span> <input type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Register</button> <span id='need'><span style='font-size: 14px;line-height: 16px;'>Have an account?</span><a id='have' href='#have' style='top: 8px; left: 0px;'>Login</a></span> </form>";
+const register = "<form action=''> <h1 style='font-size: 30px;'>Create an account</h1> <p>Explore the world</p> <span>E-mail</span> <input id='email' type='text'> <span>Password</span> <input id='password' type='password'> <span>Repeat password</span> <input id='repassword' type='password'> <a href='#forgot'>Forgot your password?</a> <button class='fullWidth'>Register</button> <span id='need'><span style='font-size: 14px;line-height: 16px; top: 20px;'>Have an account?</span><a id='have' href='#have' style='top: 8px; left: 0px;'>Login</a></span> </form>";
 //Splash
 const splash = "<div id='splash'></div>";
 
@@ -26,6 +26,7 @@ const app = "<div class='app'> <div class='before'></div>  </div>";
 const left_bar = "<div class='left_bar'><div class='before'></div></div>"
 const friends_html = "<div class='friends'><div class='po'></div></div>";
 const search_bar = "<div class='search'><span>Search friends</div>";
+let registerbar = false;
 
 function startApp(){
     $("#app").html(app);
@@ -37,7 +38,9 @@ function startApp(){
 function changePage(where){
     if(where == "Register"){
         $("#account").html(register);
+        registerbar = true;
     } else{
+        registerbar = false;
         $("#account").html(login);
     }
     clickPage();
@@ -52,6 +55,52 @@ function clickPage(){
     });
 }
 
+function validateForm(){
+    let email = $("form #email").val();
+    let password = $("form #password").val();
+    let error = "";
+    if(registerbar){
+        let repassword = $("form #repassword").val();
+        $("#email").before("<div class='error' id='error_email'>"+error+"</div>");
+        $("#password").before("<div class='error' id='error_password'>"+error+"</div>");
+        $("#repassword").before("<div class='error' id='error_repassword'>"+error+"</div>");
+        if(email == "" && password == "" && repassword == ""){
+            error = "Fill in the fields";
+            $("#error_email").html("<span>"+error+"</span>");
+            $("#error_password").html("<span>"+error+"</span>");
+            $("#error_repassword").html("<span>"+error+"</span>");
+        }
+        else if(email == ""){
+            error = "Fill in the field";
+            $("#error_email").html("<span>"+error+"</span>");
+            $("#error_password").html("");
+            $("#error_repassword").html("");
+            $("#error_email").html("<span>"+error+"</span>");
+        } else if(password == ""){
+            error = "Fill in the field";
+            $("#error_email").html("");
+            $("#error_repassword").html("");
+            $("#error_password").html("<span>"+error+"</span>");
+        } else if(repassword == ""){
+            error = "Fill in the field";
+            $("#error_email").html("");
+            $("#error_repassword").html("");
+            $("#error_repassword").html("<span>"+error+"</span>");
+        } else{
+            if(password == repassword){
+                $("#error_email").html("");
+                $("#error_password").html("");
+                $("#error_repassword").html("");
+                splashLoading();
+            } else{
+                error = "Passwords do not match";
+                $("#error_repassword").html(error);
+            }
+        }
+    }
+    console.log(error);
+}
+
 function splashLoading(){
     $("#app .before").before(splash);
     setTimeout(function(){
@@ -64,7 +113,7 @@ function splashLoading(){
 
 function submitForm(){
     $("form button").click(function(){
-        splashLoading();
+        validateForm();
         return false;
     });
 }
